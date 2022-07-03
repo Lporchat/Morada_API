8252410996de23e0d15c75f0e1f782d13aa93580
Leonardo Vinicius <lporchat06@gmail.com>
2022-07-03 00:12:37 -0300

    add de verificações na criação

--- src/modules/posts/useCases/CreatePost/CreatePostController.ts
+++ src/modules/posts/useCases/CreatePost/CreatePostController.ts
@@ -1,9 +1,14 @@
 import { Request, Response } from "express";
+import { AppError } from "../../../../shared/errors/AppError";
 import { CreatePostUseCase } from "./CreatePostUseCase";
 
 class CreatePostController {
   async handle(req: Request, res: Response): Promise<Response> {
     const { name } = req.body;
+
+    if (!name) {
+      throw new AppError("Nenhum nome inserido!");
+    }
 
     const createPostUseCase = new CreatePostUseCase();
 

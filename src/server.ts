e35bee1328d9965cca5f1de04661c4c78f51985f
Leonardo Vinicius <lporchat06@gmail.com>
2022-07-02 21:08:50 -0300

    add do swagger

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,29 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+import "reflect-metadata";
 import morgan from "morgan";
+import swaggerUI from "swagger-ui-express";
+import swaggerFile from "../swagger.json";
 
-import './src/shared/infra/typeorm';
+import "../src/shared/infra/typeorm";
+import { AppError } from "../src/shared/errors/AppError";
 
 const app = express();
 
+app.use(express.json());
 app.use(morgan("dev"));
 
-app.get("/", (req, res) => {
-  return res.json({ mensager: true });
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({ message: err.message });
+  }
+  return res.status(500).json({
+    status: "Error",
+    message: `Internal Server Error: ${err.message}`,
+  });
 });
+
 
 app.listen(3000, () => console.log("its running !!! 🙈"));

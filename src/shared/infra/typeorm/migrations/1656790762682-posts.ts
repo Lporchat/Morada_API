6c7a50f8ce735df0002160efdde793958db5edd2
Leonardo Vinicius <lporchat06@gmail.com>
2022-07-02 21:09:05 -0300

    add das migrations

--- src/shared/infra/typeorm/migrations/1656790762682-posts.ts
+++ src/shared/infra/typeorm/migrations/1656790762682-posts.ts
@@ -1,11 +1,31 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class posts1656790762682 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "posts",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "likes",
+            type: "numeric",
+            default: 0,
+          },
+        ],
+      })
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users");
+  }
 }
---
+++ src/shared/infra/typeorm/migrations/1656806510686-comments.ts
@@ -0,0 +1,45 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class comments1656806510686 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "comments",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "comment",
+            type: "varchar",
+          },
+          {
+            name: "post_id",
+            type: "uuid",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "FKPostComment",
+            referencedTableName: "posts",
+            referencedColumnNames: ["id"],
+            columnNames: ["post_id"],
+            onDelete: "SET NULL",
+            onUpdate: "SET NULL",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("comments");
+  }
+}
